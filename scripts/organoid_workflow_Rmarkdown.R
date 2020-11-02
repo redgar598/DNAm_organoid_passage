d7555afb8e3b470560f8e95637932440bc0fc7b3
Rachel Edgar <redgar598@gmail.com>
2020-11-02 11:55:59 +0000

    rerun1

--- scripts/organoid_workflow_Rmarkdown.R
+++ scripts/organoid_workflow_Rmarkdown.R
@@ -1,6 +1,6 @@
 
 
-#rmarkdown::render("scripts/01_organoids_preprocessing.R", output_dir = "output")
+rmarkdown::render("scripts/01_organoids_preprocessing.R", output_dir = "output")
 #rmarkdown::render("scripts/04_passage_CpGs.R", output_dir = "output")
 
 #rmarkdown::render("scripts/09_enrichment_heteroskedasticity_CpG.R", output_dir = "output")
@@ -11,7 +11,7 @@
 #rmarkdown::render("scripts/14_post_GO_ORA.R", output_dir = "output")
 
 #rmarkdown::render("scripts/15_cancer_beta_simplified.R", output_dir = "output")
-rmarkdown::render("scripts/16_MTAB4957_passage.R", output_dir = "output")
+#rmarkdown::render("scripts/16_MTAB4957_passage.R", output_dir = "output")
 #rmarkdown::render("scripts/17_GSE141256_organoids.R", output_dir = "output")
 #rmarkdown::render("scripts/18_GSE142213_organoids.R", output_dir = "output")
 
